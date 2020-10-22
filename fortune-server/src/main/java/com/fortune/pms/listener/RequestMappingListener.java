4a03800d580597dba89e2b580b052cb92c9867e8
parkminseob <minsub.prk@gmail.com>
2020-10-22 10:45:42 +0900

    일지수정, LogOutCommand생성,

--- fortune-server/src/main/java/com/fortune/pms/listener/RequestMappingListener.java
+++ fortune-server/src/main/java/com/fortune/pms/listener/RequestMappingListener.java
@@ -8,12 +8,12 @@ import com.fortune.pms.domain.Member;
 import com.fortune.pms.handler.CommandListCommand;
 import com.fortune.pms.handler.FortuneAddCommand;
 import com.fortune.pms.handler.FortuneResponseCommand;
-import com.fortune.pms.handler.HelloCommand;
 import com.fortune.pms.handler.MemberDeleteCommand;
 import com.fortune.pms.handler.MemberDetailCommand;
 import com.fortune.pms.handler.MemberJoinCommand;
 import com.fortune.pms.handler.MemberListCommand;
 import com.fortune.pms.handler.MemberLoginCommand;
+import com.fortune.pms.handler.MemberLogoutCommand;
 import com.fortune.pms.handler.MemberUpdateCommand;
 
 // 클라이언트 요청을 처리할 커맨드 객체를 준비한다.
@@ -34,9 +34,9 @@ public class RequestMappingListener implements ApplicationContextListener {
     context.put("/member/delete", new MemberDeleteCommand(memberList));
     context.put("/fortune/add", new FortuneAddCommand(fortuneList));
     context.put("/fortune/res", new FortuneResponseCommand(fortuneList));
-    context.put("/hello", new HelloCommand());
     context.put("/login", new MemberLoginCommand(memberList));
     context.put("/join", new MemberJoinCommand(memberList));
+    context.put("/logout", new MemberLogoutCommand());
     context.put("/command", new CommandListCommand());
   }
 
